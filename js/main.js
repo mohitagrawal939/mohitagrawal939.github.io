470ebbf9c396522e06d8097f8b8e16fb5683a41b
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-12-29 01:41:58 +0530

    Added gardient for rotating text

--- js/main.js
+++ js/main.js
@@ -140,6 +140,6 @@ window.onload = function () {
     // INJECT CSS
     var css = document.createElement("style");
     css.type = "text/css";
-    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 3px solid #fff }";
     document.body.appendChild(css);
 };
